c41d12591ba26387b2dbc6bd12f3a8422c00dbed
Guillermo Estrada <phrzn@live.com>
2022-08-16 19:55:05 -0500

    App business logic and tests

---
+++ internal/app/app.go
@@ -0,0 +1,45 @@
+package app
+
+import (
+	"crypto/sha512"
+	"encoding/base64"
+
+	"github.com/phrozen/password-hash-exercise/internal/store"
+)
+
+// 512 bits / 8 = 64 bytes * 8 / 6 = 86
+// plus padding for a multiple of 4 = 88
+const HASH_LENGTH = 88
+
+type App struct {
+	store store.Store
+}
+
+func New(s store.Store) *App {
+	return &App{store: s}
+}
+
+func (app *App) GetHash(id int) (string, error) {
+	hash, err := app.store.Get(id)
+	return string(hash), err
+}
+
+func (app *App) SetHash(password string) (int, error) {
+	hash := app.hash([]byte(password))
+	return app.store.Set(hash)
+}
+
+// Shutdown runs all tear down operations like closing the Store
+func (app *App) Shutdown() error {
+	return app.store.Close()
+}
+
+func (app *App) hash(input []byte) []byte {
+	hash := sha512.Sum512(input)
+	// Deterministic buffer size will improve
+	// performance, if slow, try sync.Pool
+	output := make([]byte, HASH_LENGTH)
+	// convert fixed size array 'hash' by slicing it
+	base64.StdEncoding.Encode(output, hash[:])
+	return output
+}
---
+++ internal/app/app_test.go
@@ -0,0 +1,68 @@
+package app
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"testing"
+	"time"
+
+	"github.com/phrozen/password-hash-exercise/internal/store"
+)
+
+// WARNING: Don't use this, use testify instead!
+// https://github.com/stretchr/testify
+// This is only good if you are limited to std library
+// and know what you are doing...
+func equal(t *testing.T, want, have any) {
+	if want != have {
+		_, f, l, _ := runtime.Caller(1)
+		t.Errorf("\n%s:%d\n\t%s\texpected: %v - got: %v", f, l, t.Name(), want, have)
+	}
+}
+
+// Test with random input sizes and data, assert the resulting
+// hash is always the expected length, regardless of input.
+func TestHash(t *testing.T) {
+	app := New(nil)
+	for i := 0; i < 100; i++ {
+		password := make([]byte, rand.Intn(64)+1)
+		rand.Read(password)
+		hash := app.hash(password)
+		equal(t, HASH_LENGTH, len(hash))
+	}
+}
+
+// Kinda redundant with just a single Store implementation,
+// just in case more Stores are added for comparison
+func TestSetGet(t *testing.T) {
+	app := New(store.NewMemory(0))
+	for i := 0; i < 100; i++ {
+		input := fmt.Sprintf("password-%d", i)
+		index, err := app.SetHash(input)
+		equal(t, err, nil)
+		equal(t, index, i)
+		// Even with 0 delay, scheduler needs time for
+		// context switching, 1ms should be enough
+		// but we use higher values for slow machines
+		time.Sleep(10 * time.Millisecond)
+		output, err := app.GetHash(i)
+		equal(t, err, nil)
+		equal(t, HASH_LENGTH, len(output))
+	}
+}
+
+// Shows consistent performance up to 64 byte inputs (common use case)
+// Implementation has consistent allocs and memory usage (deterministic)
+func BenchmarkHash(b *testing.B) {
+	app := New(nil)
+	for s := 8; s <= 1024; s = s << 1 {
+		password := make([]byte, s)
+		rand.Read(password)
+		b.Run(fmt.Sprintf("%d_bytes_input", s), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				app.hash(password)
+			}
+		})
+	}
+}
